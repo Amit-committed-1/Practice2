da0cf204bbf3968ae63d144ec827d9b8a7853fbf
Amit Kumar Bhanjbabu <amitbhanj76@gmail.com>
2025-03-17 00:01:31 +0530

    all

--- src/main/java/com/psa1/X1Application.java
+++ src/main/java/com/psa1/X1Application.java
@@ -9,6 +9,7 @@ public class X1Application {
 	String name = "Amit";
 	String emailId = "Amit@gmail.com";
 	String graduation = "BCA";
+	int age = 12;
 	public static void main(String[] args) {
 		SpringApplication.run(X1Application.class, args);
 	}
