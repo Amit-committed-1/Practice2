c6c175eecdad3412816e042ea317da8deb9b805d
Amit Kumar Bhanjbabu <amitbhanj76@gmail.com>
2025-03-17 15:32:11 +0530

    some changes regarding fields

--- src/main/java/com/psa1/X1Application.java
+++ src/main/java/com/psa1/X1Application.java
@@ -8,6 +8,7 @@ public class X1Application {
 
 	String name = "Amit Kumar Bhanjbabu";
 	int age = 25;
+	float num = 123.56f;
 	String degree = "BCA";
 	public static void main(String[] args) {
 		SpringApplication.run(X1Application.class, args);
